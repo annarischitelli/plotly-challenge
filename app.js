3d1348616ba959d3511f48e8bc7a0c4ce6e38a00
annarischitelli <annarischitelli94@gmail.com>
2021-01-20 14:46:09 -0700

    make sure data format is correct

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 // 1. Use D3 to read in samples.json
+d3.json()
+
 // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
     // 2a. Use sample_values as the values for the bar chart.
     // 2b. Use otu_ids as the labels for the bar chart.
     // 2c. Use otu_labels as the hovertext for the chart.
+
+
 // 3. Create a bubble chart that displays each sample.
     // 3a. Use otu_ids for the x values.
     // 3b. Use sample_values for the y values.
     // 3c. Use sample_values for the marker size.
     // 3d. Use otu_ids for the marker colors.
     // 3e. Use otu_labels for the text values.
+
+
 // 4. Display the sample metadata, i.e., an individual's demographic information.
+
+
 // 5. Display each key-value pair from the metadata JSON object somewhere on the page.
+
+
 // 6. Update all of the plots any time that a new sample is selected.
