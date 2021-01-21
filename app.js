fe3a4c024dd2c4954d0b53da3f057522f3faac63
annarischitelli <annarischitelli94@gmail.com>
2021-01-20 20:54:16 -0700

    attempt at gauge but epic fail

--- app.js
+++ app.js
@@ -105,3 +105,22 @@ function run() {
     })
 }
 run()
+
+
+// Attempt at gauge
+function gauge (id){
+    var gaugeData = d3.selectAll("#gauge");
+    var data = [
+        {
+            domain: { x: [0, 1], y: [0, 1] },
+            value: 10,
+            title: { text: "Speed" },
+            type: "indicator",
+            mode: "gauge+number"
+        }
+    ];
+    
+    var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+    Plotly.newPlot('myDiv', data, layout);
+}
+    
